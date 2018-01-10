6e31e32d965750a347effddf75325af702cb4914
Sander Mertens <sander.mertens8@gmail.com>
2018-01-10 06:29:52 -0800

    Initial commit

---
+++ include/si.h
@@ -0,0 +1,23 @@
+/* si.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef SI_H
+#define SI_H
+
+#include <corto/corto.h>
+#include <si/_project.h>
+#include <corto/c/c.h>
+
+/* $header() */
+/* Enter additional code here. */
+/* $end */
+
+/* $body() */
+/* Enter code that requires types here */
+/* $end */
+
+#endif
+
---
+++ include/units.h
@@ -0,0 +1,29 @@
+/* units.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef UNITS_H
+#define UNITS_H
+
+#include <corto/corto.h>
+#include <corto/corto.h>
+#include <units/_project.h>
+#include <corto/c/c.h>
+
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+#include <units/_type.h>
+#include <units/_interface.h>
+#include <units/_load.h>
+#include <units/c/_api.h>
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
+#endif
+
